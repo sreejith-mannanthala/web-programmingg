920c23b3e1f1cad59ca7dbaaf96211569e7585e6
sreejith-mannanthala <120080517+sreejith-mannanthala@users.noreply.github.com>
2023-02-22 21:39:15 +0530

    Update conn.php

--- Database/BookManagement/conn.php
+++ Database/BookManagement/conn.php
@@ -2,7 +2,7 @@
 	$servername='localhost';
 	$username='root';
 	$password='';
-	$dbname = "abhimanyu";
+	$dbname = "sreejith";
 
 	$conn=mysqli_connect($servername,$username,$password,$dbname);
 	if(!$conn){
